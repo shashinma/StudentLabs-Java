b1c87d36aca4df4453ed2efab0fbf6b300170e92
Mikhail Shashin <shashinma@icloud.com>
2022-11-29 19:06:45 +0300

    Add files via upload

---
+++ Mikhail/_lab9_2/src/Main.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String pathToEncryptedFile = args[0];
+        String pathToDecryptedFile = args[1];
+        String pathToResult = args[2];
+        String pathToEncryptedResult = args[4];
+
+        byte decryptionKey = Byte.parseByte(args[3]);
+        byte encryptionKey = Byte.parseByte(args[5]);
+
+        try (DataOutputStream dos = new DataOutputStream(new FileOutputStream(pathToDecryptedFile))) {
+            try (DataInputStream dis = new DataInputStream(new FileInputStream(pathToEncryptedFile))) {
+                while (dis.available() > 0)
+                    dos.writeByte(dis.readByte() ^ decryptionKey);
+            }
+        }
+
+        try (DataOutputStream output = new DataOutputStream(new FileOutputStream(pathToResult))) {
+            try (DataInputStream input = new DataInputStream(new FileInputStream(pathToDecryptedFile))) {
+                output.writeByte(input.readByte() * 113);
+                output.writeInt(input.readInt() + 113);
+                output.writeLong(input.readLong() - 113);
+            }
+        }
+
+        try (DataOutputStream dos = new DataOutputStream(new FileOutputStream(pathToEncryptedResult))) {
+            try (DataInputStream dis = new DataInputStream(new FileInputStream(pathToResult))) {
+                while (dis.available() > 0)
+                    dos.writeByte(dis.readByte() ^ encryptionKey);
+            }
+        }
+
+    }
+}
